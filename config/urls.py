8de13bbc080fe16e560a3c4e1f4c65d744c02ee1
elvis <kariuki.elvis@student.moringaschool.com>
2026-05-20 14:34:04 +0300

    Created the Viewsets

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,16 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("api/auth/", include("users.urls")),
+    path("api/doctors/", include("doctors.urls")),
+    path("api/patients/", include("patients.urls")),
+    path("api/appointments/", include("appointments.urls")),
+    path("api/billing/", include("billing.urls")),
+    path("api/payments/", include("payments.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
